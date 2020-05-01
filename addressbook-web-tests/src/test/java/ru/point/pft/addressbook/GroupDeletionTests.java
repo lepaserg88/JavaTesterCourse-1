b5caf7b674e4b19ae5fa508229016ee4ae80caa6
MaryMalysheva <56067578+MaryMalysheva@users.noreply.github.com>
2020-05-01 14:39:54 +0500

    Лекция 2.5 - Удаление группы

---
+++ addressbook-web-tests/src/test/java/ru/point/pft/addressbook/GroupDeletionTests.java
@@ -0,0 +1,15 @@
+package ru.point.pft.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+  @Test
+  public void testGroupDeletion() throws Exception {
+    gotoGroupPage();
+    selectGroup();
+    deleteCreaatedGroups();
+    returntoGroupPage();
+  }
+
+}
