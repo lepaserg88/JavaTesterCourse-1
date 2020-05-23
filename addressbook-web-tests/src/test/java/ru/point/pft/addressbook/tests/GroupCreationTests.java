ec9fa9a3d90368f05831a0fbd5ee55d89178f825
MaryMalysheva <56067578+MaryMalysheva@users.noreply.github.com>
2020-05-23 23:47:38 +0500

    Лекция 4.9 - Лямбда-выражения

--- addressbook-web-tests/src/test/java/ru/point/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/point/pft/addressbook/tests/GroupCreationTests.java
@@ -24,7 +24,7 @@ public class GroupCreationTests extends TestBase{
         max = g.getId();
       }
     }
-    group.setId(max);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
