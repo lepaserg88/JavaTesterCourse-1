a2fecd23fcefde2679baf74d07d0488835fb7cea
MaryMalysheva <56067578+MaryMalysheva@users.noreply.github.com>
2020-05-23 23:51:33 +0500

    4.9 - Модификация

--- addressbook-web-tests/src/test/java/ru/point/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/point/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.point.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -24,7 +25,8 @@ public class GroupCreationTests extends TestBase{
         max = g.getId();
       }
     }
-    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+
+    group.setId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
