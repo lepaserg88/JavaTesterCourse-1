b337bab0e3bb477da8bbf7b31ecfd80454a11558
MaryMalysheva <56067578+MaryMalysheva@users.noreply.github.com>
2020-06-16 00:33:17 +0500

    Контакты из файла

--- addressbook-web-tests/src/test/java/ru/point/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/point/pft/addressbook/tests/TestBase.java
@@ -9,7 +9,7 @@ import ru.point.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.IE);
+  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
